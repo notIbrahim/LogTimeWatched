89ce87bfd8854b0740c16e934121f08e6637a36a
Timekeeper <muhammdzihsan1@outlook.com>
2022-06-30 23:45:14 +0700

    This is Second Iteration - Change some logic to easy read # This is future note lambda will be use for but i don't know how to do it properly

--- app/src/main/java/com/times/logtimewatched/MainActivity.kt
+++ app/src/main/java/com/times/logtimewatched/MainActivity.kt
@@ -13,7 +13,7 @@ import com.google.firebase.ktx.Firebase
 import java.util.*
 
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity(), View.OnClickListener
 {
     private lateinit var auth: FirebaseAuth
     private lateinit var emails: EditText
@@ -25,12 +25,6 @@ class MainActivity : AppCompatActivity()
     override fun onStart() {
         super.onStart()
         auth = Firebase.auth
-        val users = auth.currentUser
-
-        if(users != null)
-        {
-            finish()
-        }
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,40 +35,49 @@ class MainActivity : AppCompatActivity()
             val noLog = Intent(this, Timewatch::class.java)
             startActivity(noLog)
         }
-
+        // initialize email and password
+        emails = findViewById(R.id.username)
+        passwords = findViewById(R.id.password)
         loginBtn = findViewById(R.id.ModalLogin)
-        loginBtn.setOnClickListener{
-            if(emails.text.toString().isNotEmpty() && passwords.text.toString().isNotEmpty()) /*(/* emails.isNotEmpty() == async.data */)*/
-            {
-                auth.signInWithEmailAndPassword(emails.text.toString(), passwords.text.toString())
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful)
-                        {
-                            val userLogin = Intent(this, Timewatch::class.java)
-                            userLogin.putExtra(Timewatch._currUsers, emails.text.toString())
-                            startActivity(userLogin)
-                        }
-                        else
-                        {
-                            Toast.makeText(applicationContext, "Either your password and Email Incorrectly", Toast.LENGTH_SHORT).show()
-                        }
+        // end initialize
 
-                    }
-            }
-            else
-            {
-                Toast.makeText(applicationContext, "If you want use this without login, Please use Login As Guess", Toast.LENGTH_SHORT).show()
-            }
-        }
+        loginBtn.setOnClickListener(this)
         createUser = findViewById(R.id.Accounts)
         createUser.setOnClickListener{
             val newUser = Intent(this, CreateAccount::class.java)
             startActivity(newUser)
         }
     }
-/*
-    override fun onClick(p0: View?) {
-        TODO("Not yet implemented")
+
+    override fun onClick(Views: View?)
+    {
+        when(Views?.id)
+        {
+            R.id.ModalLogin ->
+            {
+                if(emails.text.toString().isNotEmpty() && passwords.text.toString().isNotEmpty()) /*(/* emails.isNotEmpty() == async.data */)*/
+                {
+                    auth.signInWithEmailAndPassword(emails.text.toString(), passwords.text.toString())
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful)
+                            {
+                                val userLogin = Intent(this, Timewatch::class.java)
+                                userLogin.putExtra(Timewatch._currUsers, emails.text.toString())
+                                startActivity(userLogin)
+                            }
+                            else
+                            {
+                                Toast.makeText(applicationContext, "Either your password and Email Incorrectly", Toast.LENGTH_SHORT).show()
+                            }
+
+                        }
+                }
+                else
+                {
+                    Toast.makeText(applicationContext, "If you want use this without login, Please use Login As Guess", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     /*
@@ -82,5 +85,4 @@ class MainActivity : AppCompatActivity()
         TODO("Not Implemented")
     }
     */
- */
 }
